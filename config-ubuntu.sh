96644f8e7cadcb5f88d7e047e6d753540b4c2437
Marlon Klagenberg <marlon.klc@gmail.com>
2020-08-20 14:11:07 -0300

    Update config-ubuntu.sh

--- config-ubuntu.sh
+++ config-ubuntu.sh
@@ -11,11 +11,11 @@ msg ""
 msg "######## Ubuntu configuration #########"
 msg ""
 
-gsettings set com.canonical.Unity always-show-menus true
-msg_ok "Always show options menu"
+#gsettings set com.canonical.Unity always-show-menus true
+#msg_ok "Always show options menu"
 
-sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
-msg_ok "Show all startup application files"
+#sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
+#msg_ok "Show all startup application files"
 
 sudo sysctl vm.swappiness=10 &> /dev/null
 msg_ok "Uses swap only when memory is on 90% of capacity"
